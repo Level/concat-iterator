76d1241fded5019f506e1e1b66f594ef9bf74a33
Lars-Magnus Skog <ralphtheninja@riseup.net>
2018-06-27 19:05:40 +0200

    End iterator on next error (#10)
    * Add failing test
    
    * Call iterator.end() if .next() errors
    
    * Refactor calling iterator.end()
    
    * Better error messages

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ module.exports = function (iterator, cb) {
   var data = []
   var next = function () {
     iterator.next(function (err, key, value) {
-      if (err) return cb(err)
-      if (key === undefined && value === undefined) {
-        return iterator.end(function (err) {
-          cb(err, data)
+      if (err || (key === undefined && value === undefined)) {
+        return iterator.end(function (err2) {
+          cb(err || err2, data)
         })
       }
       data.push({ key: key, value: value })
